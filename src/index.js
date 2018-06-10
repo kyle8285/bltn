d32b81ff85dff595eb0b5d5af7dddeb83212fa6b
kyle8285 <kylelussier3@gmail.com>
2018-06-10 15:08:09 -0400

    Adding sass to the mix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './scss/base.scss';
+import './scss/type.scss';
 console.log('Hello!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin('style.css');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -16,6 +30,7 @@ module.exports = {
       template: './src/templates/past-events.html',
       filename: 'past-events/index.html',
       title: 'Past Events',
-    })
+    }),
+    extractSass,
   ]
 };
